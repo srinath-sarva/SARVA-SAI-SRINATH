4bb282bd8e700c47ea49ec264028dd8c6beb9993
sarvasaisrinath.9 <sarvasaisrinath.9@gmail.com>
2020-12-19 12:31:41 +0530

    added registration form and def register in views.py

---
+++ sarva/issues/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm 
+from django import forms 
+from .models import * 
+from django.contrib.auth.models import User
+
+class CreateUserForm(UserCreationForm):
+    phone=forms.CharField()
+    gen=(
+        ('male','male'),
+        ("female","female")
+    )
+    gender=forms.ChoiceField(choices=gen)
+    address=forms.CharField()
+    propic=forms.FileField()
+
+    class Meta:
+        model=User 
+        fields=["username","email","password1","password2","phone","gender","address","propic"]
+         
+
+         
--- sarva/issues/views.py
+++ sarva/issues/views.py
@@ -4,8 +4,30 @@ from django.http import HttpResponse
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 
-from .forms import CreateUserForm,changeform
+from .forms import CreateUserForm
 from django.contrib  import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
+def register(request):
+    
+    form=CreateUserForm()
+    
+    if request.method=="POST":
+        form=CreateUserForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save() 
+            
+            username=form.cleaned_data.get('username').lower()
+            email=form.cleaned_data.get('email')
+            phone=form.cleaned_data.get('phone')
+            user=User.objects.get(username=username)
+            gender=form.cleaned_data.get('gender')
+            propic=form.cleaned_data.get('propic')
+            address=form.cleaned_data.get('address')
+            print(user)
+            p=person(user=user,name=username,email=email,phone=phone,gender=gender,propic=propic,address=address)
+            p.save()
+            messages.info(request,"registration success")
+    context={'form':form}
+    return render(request,"issues/register.html",context) 
